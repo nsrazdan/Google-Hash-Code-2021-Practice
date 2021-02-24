8b8de58257b0de14d6327c83297cf10f76d684ea
Nikhil Razdan <nsrazdan@ucdavis.edu>
2021-02-24 17:28:02 +0000

    boiler plate code

---
+++ Google Hash Code 2021 Practice/main.py
@@ -0,0 +1,34 @@
+import sys
+
+class Pizza:
+  def __init__(self, num_ingredients):
+    self.num_ingredients = num_ingredients
+    self.ingredients = [];
+
+class Solution:
+  def __init__(self):
+    self.num_pizzas = 0
+    self.num_doub_teams = 0
+    self.num_tri_teams = 0
+    self.num_quad_teams = 0
+    self.output = "Hello world!"
+  
+  def read(self):
+    filename = str(sys.argv[0])
+    x = None
+
+  def solve(self):
+    x = None
+
+  def output(self):
+    print(self.output)
+
+  def run(self):
+    self.read()
+    self.solve()
+    self.output()
+
+
+if __name__ == "__main__":
+  sol = Solution()
+  sol.run()
